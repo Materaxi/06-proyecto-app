7b15500d7256ea747420cbc2afb64d38cdfa896d
Mateo Orihuela <mateo.orihuela@gmail.com>
2025-05-26 16:31:48 -0400

    Primer commit 06-Project-App

---
+++ src/router/index.ts
@@ -0,0 +1,26 @@
+import ProjectLayout from '@/modules/projects/layouts/ProjectLayout.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      redirect: {name: 'projects'},
+      component: ProjectLayout,
+      children: [
+        {
+          path: 'projects',
+          name: 'projects',
+          component: () => import('@/modules/projects/views/ProjectsView.vue'),
+
+        }
+      ]
+    }
+
+  ],
+})
+
+export default router
