b23591dd108742b6237bca5fd1c7af8a5544fa98
Mateo Orihuela <mateo.orihuela@gmail.com>
2025-08-03 08:46:44 -0400

    Correccion de Modal1

--- src/modules/projects/views/ProjectsView.vue
+++ src/modules/projects/views/ProjectsView.vue
@@ -1,43 +1,60 @@
 <template>
-    <div class="overflow-x-auto w-full">
-        <table class="table">
-            <!-- head -->
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>Proyecto</th>
-                    <th>Tareas</th>
-                    <th>Avance</th>
-                </tr>
-            </thead>
-            <tbody>
-                <!-- row 1 -->
+  <div class="overflow-x-auto w-full">
+    <table class="table">
+      <!-- head -->
+      <thead>
+        <tr>
+          <th></th>
+          <th>Proyecto</th>
+          <th>Tareas</th>
+          <th>Avance</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- row 1 -->
 
-                <!-- row 2 -->
-                <tr class="hover:bg-base-300">
-                    <th>2</th>
-                    <td>Hart Hagerty</td>
-                    <td>Desktop Support Technician</td>
-                    <td>Purple</td>
-                </tr>
-                <!-- row 3 -->
+        <!-- row 2 -->
+        <tr class="hover:bg-base-300">
+          <th>2</th>
+          <td>Hart Hagerty</td>
+          <td>Desktop Support Technician</td>
+          <td>Purple</td>
+        </tr>
+        <!-- row 3 -->
 
-            </tbody>
-        </table>
+      </tbody>
+    </table>
 
 
-    </div>
+  </div>
 
-    <InputModal/>
+  <InputModal :open="modelOpen" @close="modelOpen = false" @value="onNewValue"
+    placeholder="Ingrese el Nombre del proyecto" title="Nuevo Proyecto" subtitle="Ingrese el Nombre del Proyecto" />
 
-    <FabButton @click="()=>console.log('Clicked')">
-      <AddCircle/>
-    </FabButton> />
+  <custom-modal />
+
+  <FabButton position="top-right" @click="modelOpen = true">
+    <AddCircle />
+  </FabButton> />
+
+  <FabButton position="bottom-left" @click="customModalOpen = true">
+    <AddCircle />
+  </FabButton> />
+
+
 </template>
 
 <script lang="ts" setup>
 import FabButton from '@/modules/common/components/FabButton.vue';
 import AddCircle from '@/modules/common/icons/AddCircle.vue';
 import InputModal from '@/modules/common/components/InputModal.vue';
+import { ref } from 'vue';
+import CustomModal from '@/modules/common/components/CustomModal.vue';
 
+const modelOpen = ref(false);
+const customModalOpen = ref(false);
+
+const onNewValue = (projectName: string) => {
+  console.log({ projectName })
+}
 </script>
